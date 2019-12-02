cb4f8734d276bcffbaf31900176cdd07cf989815
Ben Dowen <ben@dowen.me.uk>
2019-12-02 16:28:35 +0000

    fix unit  tests

--- day1_1.py
+++ day1_1.py
@@ -24,11 +24,11 @@ def test_example_module_fuel_loads():
     assert_that(module_fuel(12)).is_equal_to(2)
     assert_that(module_fuel(14)).is_equal_to(2)
     assert_that(module_fuel(1969)).is_equal_to(654)
-    assert_that(module_fuel(100756)).is_equal_to(3358)
+    assert_that(module_fuel(100756)).is_equal_to(33583)
 
 def test_example_sum_of_fuel():
-    example_result = str(total_fuel('day1_1_testdata.txt'))
-    assert_that(example_result).is_equal_to(4016)
+    example_result = int(total_fuel('day1_1_testdata.txt'))
+    assert_that(example_result).is_equal_to(34241)
 
 real_result = str(total_fuel('day1_1.txt'))
 print(real_result)
